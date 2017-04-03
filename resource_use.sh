18661cf4959362199f48a175f1a8d76b785fdc65
Phil9S <philsmith990@hotmail.co.uk>
2017-04-03 16:52:24 +0100

    Minor adjustments to resource script output

--- resource_use.sh
+++ resource_use.sh
@@ -12,20 +12,20 @@ fi
 
 while [[ $LOOP = "TRUE" ]]; do
 
-	if [[ $(ps aux | grep "$JOB" | head -n -1 | awk -v OFS="\t" '$1=$1' | cut -f 6 | wc -l) -lt 1 ]]; then
+	if [[ $(ps aux | grep "$JOB" | grep -v "grep" | grep -v "resource_use" | awk -v OFS="\t" '$1=$1' | cut -f 6 | wc -l) -lt 1 ]]; then
 		echo -e "No Processes Found"
 		LOOP="FALSE"
 		exit
 	else
-		RES=$(ps aux | grep "$JOB" | head -n -1 | awk -v OFS="\t" '$1=$1' | cut -f 6 | sort -r | head -n 1)
+		RES=$(ps aux | grep "$JOB" | grep -v "grep" | grep -v "resource_use" | awk -v OFS="\t" '$1=$1' | cut -f 6 | sort -r | head -n 1)
 		if [[ $RES -gt $USAGE ]]; then
 			USAGE=${RES}
-			echo -e "Current maximum Usage: $(( $USAGE / 1000 ))Mb / $(( $USAGE / 1000000 ))Gb"
+			echo -e `date +[%D-%R]` "Current max usage: $(( $USAGE / 1000 ))Mb / $(( $USAGE / 1000000 ))Gb" >> $HOME/resource_use.log
 		fi	
 		sleep 3
 	fi
 done
 
 if [[ $USAGE -gt 0 ]]; then
-	echo -e "Maximum Usage: $(( $USAGE / 1000 ))Mb / $(( $USAGE / 1000000 ))Gb" | tee -a $HOME/resource.log
+	echo -e `date +[%D-%R]` "Final maximum usage: $(( $USAGE / 1000 ))Mb / $(( $USAGE / 1000000 ))Gb" >> $HOME/resource_use.log
 fi
