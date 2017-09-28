cdad75d8d05cb1e88869830b5b09ade352360741
Phil9S <philsmith990@hotmail.co.uk>
2017-09-28 17:06:24 +0100

    rework of genesearch to function from folder & addition of sex prediction tool using x/y chromosome read count

--- gene_search_BASS.sh
+++ gene_search_BASS.sh
@@ -5,18 +5,24 @@ loc=${1}
 gene=${2}
 
 if [[ $# -eq 0 ]]; then
-	echo -e "GeneSearch - Arguments please!  Try ./geneSearch /this/input/folder GENESYMBOL \nUse on BASS  annotated score files"
+	echo -e "## Gene Search ## - Arguments please!  Try ./geneSearch /this/input/folder GENESYMBOL \nUse on BASS  annotated score files"
 	exit
 fi
 
 if [[ $# -lt 2 ]]; then
-        echo -e "GeneSearch - More Arguments please!  Try ./geneSearch /this/input/folder GENESYMBOL \nUse on BASS  annotated score files"
+        echo -e "## Gene Search ## - More Arguments please!  Try ./geneSearch /this/input/folder GENESYMBOL \nUse on BASS  annotated score files"
         exit
 fi
 
+if [[ ! -d ${loc} ]]; then
+	echo -e "## Gene Search ## - Input folder provided does not exist - Exiting Now"
+	exit
+fi
+
+cd $HOME
 
 if [ -d ".genesearch_temp" ]; then
-	echo -e "GeneSearch - Temp folder exists - folder not generated\n"
+	echo -e "## Gene Search ## - Temp folder exists - folder not generated\n"
 else
 	mkdir .genesearch_temp
 fi
@@ -29,7 +35,7 @@ vim -c "%s#${loc}##g|wq" input.list
 
 echo -e "Sample	Chr	Start	End	cytoBand	Gene.refGene	Func.refGene	ExonicFunc.refGene	avsnp144	Ref	Alt	AA1	AA2	AA_Pos	Lenght	TranscriptID	Exon	Interpro_domain	X1000g2015aug_all	ExAC_ALL	esp6500siv2_all	GT	Q	PL	Depth	Score	TruncScore	HomozygScore	Polyphen2_HDIV_score	Polyphen2_HVAR_score	VEST3_score	MutationAssessor_score_Converted	GERP.._RS_Converted	phyloP7way_vertebrate_score	phyloP20way_mammalian_score	SIFT_scoreInv	fathmm.MKL_coding_score	LRT_score	MutationTaster_score	FATHMM_score_Converted	PROVEAN_score_Converted	CADD_raw_Converted	DANN_score	MetaSVM_score_Converted	MetaLR_score	integrated_fitCons_score_Converted	integrated_confidence_value_Converted	phastCons7way_vertebrate	phastCons20way_mammalian	SiPhy_29way_logOdds_Converted	dbscSNV_ADA_SCORE	dbscSNV_RF_SCORE	dpsi_max_tissue_Converted	X1000g2015aug_afr	X1000g2015aug_eas	X1000g2015aug_eur	X1000g2015aug_amr	X1000g2015aug_sas	ExAC_AFR	ExAC_AMR	ExAC_EAS	ExAC_FIN	ExAC_NFE	ExAC_OTH	ExAC_SAS	esp6500siv2_aa	esp6500siv2_ea	nci60	cosmic70	CLINSIG	CLNDBN	CLNACC	CLNDSDB	CLNDSDBID" > geneSearch_${gene}_results.txt
 
-echo -e "GeneSearch - Searching for ${gene}..."
+echo -e "## Gene Search ## - Searching for ${gene}..."
 for i in `cat input.list`; do
 	
 	cat ${loc}${i}.scores.txt | grep "$gene" > grepfile.txt
@@ -37,9 +43,18 @@ for i in `cat input.list`; do
 	cat grepfile.txt >> geneSearch_${gene}_results.txt
 
 done
-echo "GeneSearch - $(cat input.list | wc -l) total files searched..."
-echo -e "GeneSearch - Search Complete"
+echo "## Gene Search ## - $(cat input.list | wc -l) total files searched..."
 mv geneSearch_${gene}_results.txt ../
 cd ../
+
+if [ -d ".genesearch_temp" ]; then
+        echo -e "## Gene Search ## - Results folder exists - folder not generated"
+else
+        mkdir gene_search_results/
+fi
+
 mv geneSearch_${gene}_results.txt gene_search_results/
 rm -r .genesearch_temp
+
+echo -e "## Gene Search ## - Search Complete"
+
---
+++ sex_calc.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#default output set to home directory of user
+OUTPUT="${HOME}/"
+#first provided argument is directory of BAM files for analysis
+BAM_FOLDER=${1}
+
+#make sure the required arguemnt is provided
+if [[ $# -eq 0 ]]; then
+	echo -e "## Sex Calculator ## - Input folder required (e.g. - ./sex_calc.sh /folder/containing/bams/)"
+	exit
+fi
+
+# basic checks for the existence of the folder and bam files
+if [[ ! -d ${BAM_FOLDER} ]]; then
+	echo -e "## Sex Calculator ## - Input folder provided does not exist - Exiting Now"
+	exit
+fi
+if [[ `ls ${BAM_FOLDER}*.bam | wc -l` < '1' ]]; then
+	echo -e "## Sex Calculator ## - Input folder provided contains no BAM files - Exiting Now"
+	exit
+fi
+#list all bam files present in folder
+ls ${BAM_FOLDER}*.bam > bam_list_sexcalc.txt
+#setup output file
+echo -e "Sample\tX/Y_ratio\tPredicted_sex" > ${OUTPUT}sex_calc_results.tsv
+#report script is running
+echo -e "## Sex Calculator ## - Predicting biological sex using X/Y read ratio over $(cat bam_list_sexcalc.txt | wc -l) bam files..."
+
+#for each bam file listed, do
+#calculate total read numbers on chrX and chrY - calculate the ratio of reads on X compared to Y
+#high ratio (e.g. more than 30) likely female - low ratio (e.g. less than 10) likely male - Inbetween values are deliberately left as unknown
+COUNTER=0
+echo -e "## Sex Calculator ## - Calculating predictive Sex - In progress... 0.0%"
+for i in `cat bam_list_sexcalc.txt`; do
+	## progress check
+	let COUNTER=COUNTER+1
+	FINAL_PROG=$(cat bam_list_sexcalc.txt | wc -l)
+	COUNTER_PCT=$(( COUNTER * 100 ))
+	PCT=$(bc <<< "scale=1; $COUNTER_PCT / $FINAL_PROG")
+	
+	## ratio calculator	
+	X=$(samtools view -c ${i} chrX 2> /dev/null)
+	Y=$(samtools view -c ${i} chrY 2> /dev/null) 
+	ratio=$(( X / Y ))
+	
+	if [[ "$ratio" -le 10 ]]; then
+		echo -e "${i}\t${ratio}\tM" >> ${OUTPUT}sex_calc_results.tsv
+	elif [[ "$ratio" -ge 30 ]]; then
+		echo -e "${i}\t${ratio}\tF" >> ${OUTPUT}sex_calc_results.tsv
+	else
+		echo -e "${i}\t${ratio}\t?" >> ${OUTPUT}sex_calc_results.tsv
+	fi
+	#progress reporting
+	echo -e "## Sex Calculator ## - Calculating predictive Sex - In progress... ${PCT}%"
+done
+
+#trim the file names in the output to just the sample and not the entire file path
+vim -c "%s%/\S\+/%%g|wq" ${OUTPUT}sex_calc_results.tsv
+vim -c "%s%_hg38\S\+%%g|wq" ${OUTPUT}sex_calc_results.tsv
+
+#remove temp files
+rm bam_list_sexcalc.txt
+echo -e "## Sex Calculator ## - Script finished - Results default to home folder"
