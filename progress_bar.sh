a2bd761441e82cc1cb161b2bf9b7132a097cdad1
Philip Smith <philsmith990@hotmail.co.uk>
2021-05-14 18:47:19 +0100

    Code refactor
    Refactor of progress bar code, including replacement of for loop with while read.

--- progress_bar.sh
+++ progress_bar.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-
 PROG=1
-TOTAL=$(cat FILE | wc -l)
+FILE="file_summary.txt"
+TOTAL=$(cat ${FILE} | wc -l)
+SCRIPT_NAME=$0
 
-for i in `cat FILE`; do
+while read -r i; do
+        PROG_PCT=$(echo "scale=2;($PROG/$TOTAL)*100" | bc | sed 's/\.[0-9][0-9]//')
+        PROG_2=$(echo "scale=4;($PROG/$TOTAL*100)" | bc)
+        PROG_f=$(echo "scale=2;($PROG_2/1)" | bc)
+        FULL=100
+        printf "${SCRIPT_NAME} |"
+        for ((done=0; done<${FULL}; done++)); do
+                if [[ "$done" -lt "$PROG_PCT" ]]; then
+                        printf "▊"
+                else
+                        printf " "
+                fi
+        done
+        printf "| ${PROG_f}%% (${PROG}/${TOTAL})\r"
 
-	PROG_PCT=$(echo "scale=2;($PROG/$TOTAL)*100" | bc | sed 's/\.[0-9][0-9]//')
-  PROG_2=$(echo "scale=4;($PROG/$TOTAL*100)" | bc)
-  PROG_f=$(echo "scale=2;($PROG_2/1)" | bc)
-	FULL=100
-	printf "PROGRESS BAR |"
-	for ((done=0; done<${FULL}; done++)); do 
-		if [[ "$done" -lt "$PROG_PCT" ]]; then
-			printf "▊"
-		else	
- 			printf " "
-		fi
-	done
-	printf "| ${PROG_f}%%(${PROG}/${TOTAL})\r"
-	
-	## LOOP FUNCTION HERE
+        ## LOOP FUNCTION HERE
 
-	
-	sleep 1
-	let PROG++
-done
+        #sleep 1
+        let PROG++
+done < ${FILE}
 echo -e "\r"
